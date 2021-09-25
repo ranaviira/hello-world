c23311143fdce31ff41159eaf1f7f7ced8fe91d0
ranaviira <84rent@gmail.com>
2021-09-25 15:33:56 +0400

    init

---
+++ src/company/Main.java
@@ -0,0 +1,10 @@
+package company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String hello = "Hello";
+        String world = "world";
+        System.out.println(hello + " " + world + "!");
+    }
+}
